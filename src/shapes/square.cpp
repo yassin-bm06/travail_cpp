7edd544bfb81bac8b6970e7bd1f44ff307de01fe
JADDDDD-K <jad.abi-kaedbey@universite-paris-saclay.fr>
2026-05-12 10:35:40 +0200

    longueursquare.cpp
    calcul de la longueur d'un carré d apres le diagonale qui relie les points opposés (pythagore)

--- src/shapes/square.cpp
+++ src/shapes/square.cpp
@@ -1,4 +1,15 @@
 #include "point.hpp"
 #include "shapes/square.hpp"
+#include "draw.hpp" // pour draw_picture
+#include <cmath> //  pour std::sqrt, std::cos, std::sin
+#include <vector> // pour les listes de points
 
 Square::Square(Point P, Point R) : A(P), C(R) {};
+
+// Calcule la longueur d'un côté.
+//Par Pythagore, Diagonale = Côté * racine(2).
+//Côté = Diagonale / racine(2).
+double Square::side() {
+    double diagonale = A.distance(C);
+    return diagonale / std::sqrt(2.0);
+}
