8035fe716218a15e1af962e431d21e6f984f4de9
yassin-bm06 <yassim.ben-mansour@universite-paris-saclay.fr>
2026-05-12 10:14:50 +0200

    Implement area method in Circle class
    Added area method to calculate the circle's area.

--- src/shapes/circle.cpp
+++ src/shapes/circle.cpp
@@ -2,3 +2,10 @@
 #include "shapes/circle.hpp"
 
 Circle::Circle(double r, Point c) : radius(r), center(c) {};
+
+
+double Circle::area() {       # Calcul de surface de cercle
+	
+	return M_PI*radius*radius;
+
+}
