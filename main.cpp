d5610fad8c056fe99c9f65dff86def64c3b0f529
Yassim Ben Mansour <yassim.ben-mansour@universite-paris-saclay.fr>
2026-05-12 11:34:13 +0200

    Ajouté les methodes resize() et equals() de Circle après avoir tout compilé

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "point.hpp"
 #include "shapes.hpp"
 #include <vector>
-
+#include <iostream>
 
 int main() {
 	
@@ -18,6 +18,11 @@ int main() {
 	
 	// Draw the picture by connecting the points
 	draw_picture(points);
-	
+	Circle C(5, Point(0,0));
+	Circle E(5, Point(1,0));
+	if(not C.equals(E)){
+		std::cout<< "not OK";
+	}
 	return 0;
+
 }
