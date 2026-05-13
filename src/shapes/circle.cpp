e25c2331c2f156841b8c1b86bba540bf5379203b
yassin-bm06 <yassim.ben-mansour@universite-paris-saclay.fr>
2026-05-13 09:57:00 +0100

    Documentation de l'ensemble des methods de la structure Circle
    Ajouté de documentation à chaque méthode de Circle déjà compilée et testée (sauf Circle.translate et Circle.draw)

--- src/shapes/circle.cpp
+++ src/shapes/circle.cpp
@@ -11,10 +11,21 @@ using namespace std;
 Circle::Circle(double r, Point c) : radius(r), center(c) {};
 
 
-double Circle::circumference() {  //calculer la circonférence du cercle 
+
+/*
+@La methode Circle.circumference
+@return en type double, la circomference du cercle en fonction de son rayon
+*/
+
+double Circle::circumference() { 
 	return 2*numbers::pi*radius;
 	
 }
+
+/*
+@La methode Circle.area
+@return en type double, l'aire du cercle calculé en fonction du rayon déjà donné
+*/
 
 double Circle::area() {       //Calcul de surface de cercle
 	
@@ -22,11 +33,22 @@ double Circle::area() {       //Calcul de surface de cercle
 
 }
 
+
+/*
+@La methode Circle.resize
+@param ratio de type double, un réel (Positif)
+@(void: pas de return), modifie le rayon du cercle en le multipliant par le ratio (ratio > 1: angrandit le cercle/ ratio < 0: cercle plus petit)  
+*/
 void Circle::resize(double ratio){
 	radius = radius * ratio;
 
 }
 
+/*
+@La methode Circle.equals
+@param circle de type Circle
+@return un booléen, vérifie si circle est égale au cercle(instance courante) ou non, compare les centres ET les rayons 
+*/
 bool Circle::equals(Circle circle){
 	return (radius==circle.radius && center.x == circle.center.x && center.y == circle.center.y);
 	
